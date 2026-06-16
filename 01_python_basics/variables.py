f9472dfa807786bc87bfd7ed899d4537ac156259
Armaan <armaankushofficial@gmail.com>
2026-06-16 18:48:45 +0530

    updated variables file after rearning new variable concept

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -1,5 +1,9 @@
-name = "Armaan"
-age = 18
-
-print(name)
-print(age)
+Rent=1200
+gas=202.5
+groceries=303.5
+total=Rent+gas+groceries
+print(total)
+item1="Rent"
+item2="gas"
+item3="groceries"
+print("expensive items:",item1,item2,item3)
