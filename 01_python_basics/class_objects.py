0e728a3a1141e0ad9a92066b29ec0b2887357e59
Armaan <armaankushofficial@gmail.com>
2026-06-30 12:55:14 +0530

    Added class_objects.py

--- 01_python_basics/class_objects.py
+++ 01_python_basics/class_objects.py
@@ -1,24 +1,4 @@
-# class Human:
-#     def __init__(self,n,o):
-#         self.name=n
-#         self.occupation=o
 
-#     def do_work(self):
-#         if self.occupation=="tennis player":
-#             print(self.name,"plays tennis")
-#         if self.occupation=="actor":
-#             print(self.name,"shoots a film")
-    
-#     def speaks(self):
-#         print(self.name,"says how are you?")
-
-# tom=Human("Tom cruise","actor")
-# tom.do_work()
-# tom.speaks()
-
-# maria=Human("Maria Sharapova","tennis player")
-# maria.do_work()
-# maria.speaks()
 
 
 
